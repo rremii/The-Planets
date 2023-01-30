3f4ceaad5c15560f6c8c9a8793dbaa6d87345433
rremii <88894948+rremii@users.noreply.github.com>
2023-01-30 17:53:02 +0300

    initial commit

---
+++ src/pages/Main/Main.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components"
+//TODO use alies
+const MainPage = () => {
+
+  return <MainPageLayout>
+
+  </MainPageLayout>
+
+}
+export default MainPage
+const MainPageLayout = styled.div`
+
+
+
+`
